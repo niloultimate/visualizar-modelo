d3657dcc5292a6152edd4e6a124989baaa34ee86
Danilo Motta Hidalgo <nilo.ultimate@gmail.com>
2022-01-23 01:58:25 -0300

    fixes #4 considerando mesclagem na montagem

--- js/site.js
+++ js/site.js
@@ -88,15 +88,111 @@ function AtualizarModelo(){
 
     if(dados != null){
 
-        var result = '<table class="table" style="border: 1px solid black;"><thead><tr style="background-color:' + document.getElementById('colorCabecalhoBackground').value + ';color:' + document.getElementById('colorCabecalhoFonte').value + '">';
+        var result = '';
         
         var auxCamposExibir = $('#ddlCampos').select2('data').map(i => i.id);
+        var auxMesclagem = $('#ddlMesclagem').select2('data').map(i => i.id);
 
-        for (let i = 0; i < auxCamposExibir.length; i++) {
-            result += '<th scope="col">' + dados[0][auxCamposExibir[i]] + '</th>';
+        var auxListaMesclagem = [];
+        for (let i = 0; i < auxMesclagem.length; i++) {
+            auxListaMesclagem.push([]); 
+        }
+  
+        //Pegar tipo de lista mesclagem
+        for (let row = 1; row < dados.length; row++) {
+            var auxAdd = [];
+
+            for (let i = 0; i < auxMesclagem.length; i++) {
+                auxAdd.push(true);
+            }
+
+            for (let auxI = 0; auxI < auxAdd.length; auxI++) {
+                for (let i = 0; i < auxListaMesclagem[auxI].length; i++) {
+                    if(auxListaMesclagem[auxI][i] == dados[row][auxMesclagem[auxI]] 
+                        | dados[row][auxMesclagem[auxI]] == '' 
+                        | dados[row][auxMesclagem[auxI]] == undefined){
+                        auxAdd[auxI] = false;
+                    }
+                }
+            }
+
+            for (let auxI = 0; auxI < auxAdd.length; auxI++) {
+                if(auxAdd[auxI]){
+                    auxListaMesclagem[auxI].push(dados[row][auxMesclagem[auxI]]);
+                }
+            }
+        }
+        
+        if(auxListaMesclagem.length <= 0){
+            alert('Selecione dois tipos de mesclagem');
+            return;
         }
 
-        result += '</tr></thead>';
+        //Montar tabela
+        for (let i = 0; i < auxListaMesclagem[0].length; i++) {
+
+            //Montar cabeçalho
+            result += '<table class="table" style="border: 1px solid black;">'; 
+            result += '<thead><tr style="background-color:' + document.getElementById('colorCabecalhoBackground').value + ';color:' + document.getElementById('colorCabecalhoFonte').value + '">';
+            for (let i = 0; i < auxCamposExibir.length; i++) {
+                result += '<th scope="col">' + dados[0][auxCamposExibir[i]] + '</th>';
+            }
+            result += '</tr></thead><tbody>';
+
+            var auxPrimeiraLinha = '';
+            var auxLinha = '';
+            var auxLinhaIncluida = new Object();
+            for (let row = 1; row < dados.length; row++) {
+                if(auxListaMesclagem[0][i] == dados[row][0]){
+                    var auxIncluir = false;
+                    for (let mesc = 1; mesc < auxListaMesclagem.length; mesc++) {
+                        for (let mescCol = 0; mescCol < auxListaMesclagem[mesc].length; mescCol++) {
+                            if(auxListaMesclagem[mesc][mescCol] == dados[row][auxMesclagem[mesc]]){
+                                var auxSubIncluir = true;
+                                if(mesc in auxLinhaIncluida){
+                                    if(mescCol in auxLinhaIncluida[mesc]){
+                                        auxSubIncluir = false;
+                                    }else{
+                                        auxLinhaIncluida[mesc][mescCol] = false;
+                                    }
+                                }else{
+                                    auxLinhaIncluida[mesc] = {};
+                                    auxLinhaIncluida[mesc][mescCol] = false;
+                                }
+                                if(auxSubIncluir){
+                                    auxIncluir = true;
+                                    mescCol = auxListaMesclagem[mesc].length;
+                                }
+                            }
+                        }
+                        if(auxIncluir){ mesc = auxListaMesclagem.length }
+                    }
+                    if(auxIncluir){
+                        if(auxPrimeiraLinha == ''){
+                            auxPrimeiraLinha += '<tr>';
+                            for (let col = 0; col < auxCamposExibir.length; col++) {
+                                auxPrimeiraLinha += '<td>' + dados[row][auxCamposExibir[col]] + '</td>';
+                            }
+                            auxPrimeiraLinha += '</tr>';
+                        }else{
+                            auxLinha += '<tr>';
+                            for (let col = 0; col < auxCamposExibir.length; col++) {
+                                auxLinha += '<td>' + dados[row][auxCamposExibir[col]] + '</td>';
+                            }
+                            auxLinha += '</tr>';
+                        }
+                    }
+                }
+            }
+            console.log(auxLinhaIncluida);
+            
+            result += auxPrimeiraLinha + auxLinha;
+            result += '</tbody>';
+            result += '</table>';
+            
+            result += SepararTabela();
+        }
+
         result += SepararTabela();
 
         document.getElementById("divResult").innerHTML = result
